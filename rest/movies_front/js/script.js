12c8cdd490fd666c4093f53cad759fcb4f0631b0
Maciej Szumowski <mpszumowski@gmail.com>
2017-12-18 15:22:11 +0100

    zaczątek frontu

--- rest/movies_front/js/script.js
+++ rest/movies_front/js/script.js
@@ -1,3 +1,29 @@
 /**
  * Created by maciej on 18.12.17.
  */
+
+
+$(document).ready(function() {
+    $.ajax({
+        url: 'http://127.0.0.1:8000/movies/'
+    }).done(function(data) {
+        var ul = $("ul");
+        for (var i = 0; i < data.length; i++) {
+            var movieInfo = $(`
+                <ul>
+                    <div class="list-group-item list-group-item-action" 
+                    data-id="${data[i].id}>
+                        <span class="title">${data[i].title} 
+                        (${data[i].year})</span>
+                        <span class="description">${data[i].description}</span>
+                        <ul>
+                            <li>Dyrekcja: ${data[i].director}</li>
+                            <li>Obsada: ${data[i].cast}</li>
+                        </ul>
+                    </div>
+                </ul>
+            `);
+            ul.append(movieInfo);
+        }
+    });
+});
